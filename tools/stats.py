41d94329c09d21226bab5ca524802460e0c77e9a
George Foot <george.foot@gmail.com>
2025-10-08 03:17:54 +0100

    Make stats.py combine auipc+addi+jal and similar sequences, for better understanding of need for longer immediate widths

--- tools/stats.py
+++ tools/stats.py
@@ -2,11 +2,61 @@ import sys
 
 import assem
 
+def arg2int(arg):
+	if arg.startswith("x"):
+		value = int(arg[1:])
+	elif arg.startswith("$"):
+		value = int(arg[1:],16)
+	else:
+		value = int(arg)
+	return value
 
 if __name__ == "__main__":
 
 	filename = sys.argv[1]
 	result, entry = assem.Assembler().assemble(filename)
+
+	done = False
+	while not done:
+		done = True
+		pinstr,pargtypes,pargs = None,None,None
+		for i,(instr,argtypes,args) in enumerate(result):
+			if pinstr == "auipc" and instr == "addi":
+				if pargs[0] == args[0] and args[0] == args[1]:
+					nargs = [args[0],pargs[1]+args[2]]
+					if nargs[1] >= 0x8000:
+						nargs[1] -= 0x10000
+					result[i-1:i+1] = [("auipc_addi", "ri", nargs)]
+					done = False
+					break
+			if pinstr == "auipc_addi" and instr == "jalr":
+				if pargs[0] == args[0] and args[0] == args[1]:
+					assert args[2] == 0
+					nargs = [args[0],pargs[1]]
+					result[i-1:i+1] = [("jal_l", "ri", nargs)]
+					done = False
+					break
+			if pinstr == "auipc_addi" and instr == "jr":
+				if pargs[0] == args[0]:
+					assert args[1] == 0
+					nargs = [pargs[1]]
+					result[i-1:i+1] = [("j_l", "i", nargs)]
+					done = False
+					break
+			if pinstr == "beq" and instr == "j" and pargs[2] == 4:
+				result[i-1:i+1] = [("bne_l", "rri", pargs[:2] + args)]
+				done = False
+				break
+			if pinstr == "bne" and instr == "j" and pargs[2] == 4:
+				result[i-1:i+1] = [("beq_l", "rri", pargs[:2] + args)]
+				done = False
+				break
+			pinstr,pargtypes,pargs = instr,argtypes,args
+
+		# auipc addi load/store
+		# auipc addi jalr
+		# auipc addi jr
+		# bXXX j
 
 	loranges = {}
 
@@ -17,6 +67,7 @@ if __name__ == "__main__":
 	hiinstrs = set(["lui", "auipc"])
 
 	for instr, argtypes, args in result:
+
 		if instr == "none" or instr == "data":
 			continue
 
@@ -40,10 +91,10 @@ if __name__ == "__main__":
 		if instr in hiinstrs:
 			pass
 		else:
-			hibs = 0
+			hibs = 1
 			for i in range(16):
 				if ((imm >> i) & 1) ^ (imm < 0):
-					hibs = i
+					hibs = i+2
 			if instr not in hiranges.keys():
 				hiranges[instr] = [imm, imm, hibs]
 			else:
@@ -59,4 +110,5 @@ if __name__ == "__main__":
 		lo,hi,hibs = hiranges[instr]
 		freq = frequencies[instr]
 		print(f"{instr:<6}   {freq:5} {lo:5}  {hi:5}  {hibs:5}")
+
 
